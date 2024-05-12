9b03175be7328c8471b1e50a1ea9baef58c357cd
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-05-12 08:23:44 -0400

    updated report detail

--- vulnrichment.go
+++ vulnrichment.go
@@ -12,15 +12,73 @@ import (
 
 // CVEData represents the structure of the CVE JSON data
 type CVEData struct {
+	DataType    string `json:"dataType"`
+	DataVersion string `json:"dataVersion"`
+	CveMetadata struct {
+		CveId             string `json:"cveId"`
+		AssignerOrgId     string `json:"assignerOrgId"`
+		State             string `json:"state"`
+		AssignerShortName string `json:"assignerShortName"`
+		DateReserved      string `json:"dateReserved"`
+		DatePublished     string `json:"datePublished"`
+		DateUpdated       string `json:"dateUpdated"`
+	} `json:"cveMetadata"`
 	Containers struct {
 		CNA struct {
-			DatePublic   string `json:"datePublic"`
+			ProviderMetadata struct {
+				OrgId       string `json:"orgId"`
+				ShortName   string `json:"shortName"`
+				DateUpdated string `json:"dateUpdated"`
+			} `json:"providerMetadata"`
+			Title        string `json:"title"`
+			ProblemTypes []struct {
+				Descriptions []struct {
+					Type        string `json:"type"`
+					CweId       string `json:"cweId"`
+					Lang        string `json:"lang"`
+					Description string `json:"description"`
+				} `json:"descriptions"`
+			} `json:"problemTypes"`
+			Affected []struct {
+				Vendor   string `json:"vendor"`
+				Product  string `json:"product"`
+				Versions []struct {
+					Version string `json:"version"`
+					Status  string `json:"status"`
+				} `json:"versions"`
+				Modules []string `json:"modules"`
+			} `json:"affected"`
 			Descriptions []struct {
 				Lang  string `json:"lang"`
 				Value string `json:"value"`
 			} `json:"descriptions"`
+			Metrics []struct {
+				CvssV3_1 struct {
+					Version      string  `json:"version"`
+					BaseScore    float64 `json:"baseScore"`
+					VectorString string  `json:"vectorString"`
+					BaseSeverity string  `json:"baseSeverity"`
+				} `json:"cvssV3_1"`
+				CvssV3_0 struct {
+					Version      string  `json:"version"`
+					BaseScore    float64 `json:"baseScore"`
+					VectorString string  `json:"vectorString"`
+					BaseSeverity string  `json:"baseSeverity"`
+				} `json:"cvssV3_0"`
+				CvssV2_0 struct {
+					Version      string  `json:"version"`
+					BaseScore    float64 `json:"baseScore"`
+					VectorString string  `json:"vectorString"`
+				} `json:"cvssV2_0"`
+			} `json:"metrics"`
+			Timeline []struct {
+				Time  string `json:"time"`
+				Lang  string `json:"lang"`
+				Value string `json:"value"`
+			} `json:"timeline"`
 			References []struct {
-				URL  string   `json:"url"`
+				Url  string   `json:"url"`
+				Name string   `json:"name"`
 				Tags []string `json:"tags"`
 			} `json:"references"`
 		} `json:"cna"`
@@ -33,6 +91,23 @@ type CVEData struct {
 					} `json:"content"`
 				} `json:"other"`
 			} `json:"metrics"`
+			Affected []struct {
+				Cpes     []string `json:"cpes"`
+				Vendor   string   `json:"vendor"`
+				Product  string   `json:"product"`
+				Versions []struct {
+					Status          string `json:"status"`
+					Version         string `json:"version"`
+					VersionType     string `json:"versionType"`
+					LessThanOrEqual string `json:"lessThanOrEqual"`
+				} `json:"versions"`
+				DefaultStatus string `json:"defaultStatus"`
+			} `json:"affected"`
+			ProviderMetadata struct {
+				ShortName   string `json:"shortName"`
+				OrgId       string `json:"orgId"`
+				DateUpdated string `json:"dateUpdated"`
+			} `json:"providerMetadata"`
 		} `json:"adp"`
 	} `json:"containers"`
 }
@@ -107,27 +182,61 @@ func generateReport(data []byte) {
 		os.Exit(1)
 	}
 
-	fmt.Println("# CVE Report")
-	fmt.Printf("## CVE ID: %s\n", cveData.Containers.CNA.Descriptions[0].Value)
-	fmt.Printf("### Date Published: %s\n", cveData.Containers.CNA.DatePublic)
+	fmt.Println("# CVE Detailed Report")
+	fmt.Printf("## CVE ID: %s\n", cveData.CveMetadata.CveId)
+	fmt.Printf("### State: %s\n", cveData.CveMetadata.State)
+	fmt.Printf("### Published on: %s\n", cveData.CveMetadata.DatePublished)
+
 	fmt.Println("### Description")
 	for _, desc := range cveData.Containers.CNA.Descriptions {
-		fmt.Printf("- %s\n", desc.Value)
+		fmt.Printf("- %s (%s)\n", desc.Value, desc.Lang)
 	}
+
+	fmt.Println("### Affected Components")
+	for _, aff := range cveData.Containers.CNA.Affected {
+		fmt.Printf("- Vendor: %s\n- Product: %s\n", aff.Vendor, aff.Product)
+		for _, ver := range aff.Versions {
+			fmt.Printf("- Version: %s\n- Status: %s\n", ver.Version, ver.Status)
+		}
+		for _, mod := range aff.Modules {
+			fmt.Printf("- Module: %s\n", mod)
+		}
+	}
+
+	fmt.Println("### Problem Types")
+	for _, prob := range cveData.Containers.CNA.ProblemTypes {
+		for _, desc := range prob.Descriptions {
+			fmt.Printf("- %s: %s (%s)\n", desc.CweId, desc.Description, desc.Lang)
+		}
+	}
+
+	fmt.Println("### CVSS Scores")
+	for _, met := range cveData.Containers.CNA.Metrics {
+		if met.CvssV3_1.BaseScore != 0 {
+			fmt.Printf("- CVSS v3.1: %s, Score: %.1f, Severity: %s\n", met.CvssV3_1.VectorString, met.CvssV3_1.BaseScore, met.CvssV3_1.BaseSeverity)
+		}
+		if met.CvssV3_0.BaseScore != 0 {
+			fmt.Printf("- CVSS v3.0: %s, Score: %.1f, Severity: %s\n", met.CvssV3_0.VectorString, met.CvssV3_0.BaseScore, met.CvssV3_0.BaseSeverity)
+		}
+		if met.CvssV2_0.BaseScore != 0 {
+			fmt.Printf("- CVSS v2.0: %s, Score: %.1f\n", met.CvssV2_0.VectorString, met.CvssV2_0.BaseScore)
+		}
+	}
+
+	fmt.Println("### CPEs")
+	for _, adp := range cveData.Containers.ADP {
+		for _, aff := range adp.Affected {
+			fmt.Printf("- CPE: %s, Vendor: %s, Product: %s, Version: %s\n", strings.Join(aff.Cpes, ", "), aff.Vendor, aff.Product, aff.Versions[0].Version)
+		}
+	}
+
 	fmt.Println("### References")
 	for _, ref := range cveData.Containers.CNA.References {
-		fmt.Printf("- [%s](%s)\n", ref.URL, ref.URL)
+		fmt.Printf("- [%s](%s) (%s)\n", ref.Name, ref.Url, strings.Join(ref.Tags, ", "))
 	}
-	if len(cveData.Containers.ADP) > 0 && len(cveData.Containers.ADP[0].Metrics) > 0 {
-		fmt.Println("### Exploitation Status")
-		for _, metric := range cveData.Containers.ADP[0].Metrics {
-			if metric.Other.Type == "ssvc" {
-				if metric.Other.Content.Exploitation != "" {
-					fmt.Printf("- Exploitation: %s\n", metric.Other.Content.Exploitation)
-				} else {
-					fmt.Println("- Exploitation: N/A")
-				}
-			}
-		}
+
+	fmt.Println("### Timeline")
+	for _, t := range cveData.Containers.CNA.Timeline {
+		fmt.Printf("- %s: %s (%s)\n", t.Time, t.Value, t.Lang)
 	}
 }
